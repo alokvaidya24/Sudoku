9bb073625b47e9b883a3237473f701b287abbc80
Alok Vaidya <alokv@protonmail.com>
2021-07-13 10:11:44 +0530

    Initial Commit

---
+++ src/IntegersTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by alokvaidya on 11/07/21.
+ */
+public class IntegersTest {
+    public static void main(String [] args){
+        int i=8;
+        System.out.println(i/3);
+        i=i/3;
+        System.out.println(i);
+    }
+}
